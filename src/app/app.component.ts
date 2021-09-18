4c4326413c478f9c97ee8ea8abb82dda06a7e8db
stanislavBelinskyi <belinskyi@qarea.us>
2021-09-18 19:53:59 +0300

    run script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AddScriptsService} from "./core/services/add-scripts.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'scully-test';
+export class AppComponent implements OnInit {
+
+  constructor(private scriptService: AddScriptsService,
+              public router: Router) {
+  }
+
+  ngOnInit() {
+    this.scriptService.load('storyblock').then(data => {
+      // @ts-ignore
+      const {StoryblokBridge, location} = window
+      const storyblokInstance = new StoryblokBridge();
+
+      storyblokInstance.on(['published', 'change'], () => {
+        // reload page if save or publish is clicked
+        location.reload(true);
+      })
+    }).catch(error => console.log(error));
+  }
 }
